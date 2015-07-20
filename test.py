62382297c4ed6825cd835953fa2507f747a704f1
Aanand Prasad <aanand.prasad@gmail.com>
2015-07-20 20:55:58 +0100

    Move download() into utils.download

--- test.py
+++ test.py
@@ -1,12 +1,7 @@
 import sys
-import os
-import tempfile
-from urllib.request import urlopen
-from urllib.error import HTTPError
 
 from utils.images import search, debug, image_url, image_mime_type
-
-TMP_DIR = './tmp'
+from utils.download import download
 
 MIN_FRAMES_THEN = 3
 MAX_FRAMES_THEN = 6
@@ -57,29 +52,6 @@ def download_frame(item, prefix=None):
     mime_type = image_mime_type(item).partition('/')[2] or None
     suffix = ('.' + mime_type if mime_type else None)
     return download(image_url(item), prefix=prefix, suffix=suffix)
-
-
-def download(url, prefix=None, suffix=None):
-    if not os.path.exists(TMP_DIR):
-        os.mkdir(TMP_DIR)
-
-    local_file = tempfile.NamedTemporaryFile(
-        dir=TMP_DIR,
-        prefix=(prefix or ''),
-        suffix=(suffix or ''),
-        delete=False,
-    )
-
-    try:
-        print('Downloading {} to {}'.format(url, local_file.name))
-        local_file.write(urlopen(url).read())
-        local_file.close()
-        return local_file.name
-    except HTTPError as e:
-        print(e)
-        local_file.close()
-        os.remove(local_file.name)
-        return None
 
 
 if __name__ == '__main__':
---
+++ utils/download.py
@@ -0,0 +1,30 @@
+import os
+import tempfile
+
+from urllib.request import urlopen
+from urllib.error import HTTPError
+
+TMP_DIR = './tmp'
+
+
+def download(url, prefix=None, suffix=None):
+    if not os.path.exists(TMP_DIR):
+        os.mkdir(TMP_DIR)
+
+    local_file = tempfile.NamedTemporaryFile(
+        dir=TMP_DIR,
+        prefix=(prefix or ''),
+        suffix=(suffix or ''),
+        delete=False,
+    )
+
+    try:
+        print('Downloading {} to {}'.format(url, local_file.name))
+        local_file.write(urlopen(url).read())
+        local_file.close()
+        return local_file.name
+    except HTTPError as e:
+        print(e)
+        local_file.close()
+        os.remove(local_file.name)
+        return None
