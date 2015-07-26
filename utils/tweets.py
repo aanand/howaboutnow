330e0939dd60e6861a0076ab7faffdee36460597
Aanand Prasad <aanand.prasad@gmail.com>
2015-07-26 11:52:03 +0100

    Ignore URLs

--- utils/tweets.py
+++ utils/tweets.py
@@ -3,16 +3,16 @@ from itertools import takewhile, dropwhile
 
 def extract_query(text):
     words = text.split()
-    words = dropwhile(is_username, words)
-    words = takewhile(is_not_username, words)
+    words = dropwhile(is_not_word, words)
+    words = takewhile(is_word, words)
     words = list(words)
 
     return " ".join(words) if words else None
 
 
-def is_username(word):
-    return word.startswith('@')
+def is_word(word):
+    return not word.startswith('@') and not word.startswith('http')
 
 
-def is_not_username(word):
-    return not is_username(word)
+def is_not_word(word):
+    return not is_word(word)
