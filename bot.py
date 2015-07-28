f0d748240d82fa164f764d78ee6448a0f09cf5cb
Aanand Prasad <aanand.prasad@gmail.com>
2015-07-29 00:54:23 +0100

    Up the reply threshold

--- bot.py
+++ bot.py
@@ -49,7 +49,7 @@ class HowAboutNow(TwitterBot):
         self.config['ignore_timeline_retweets'] = True
 
         # max number of times to reply to someone within the moving window
-        self.config['reply_threshold'] = 3
+        self.config['reply_threshold'] = 5
 
         # length of the moving window, in seconds
         self.config['recent_replies_window'] = 20*60
